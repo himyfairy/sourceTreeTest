5ce4d5b2b2575a93672620ce2c1a563086e20cc7
himyfairy <himyfairy@gmail.com>
2017-09-14 16:48:14 +0800

    add "cocoapod"

---
+++ sourceTreeTest/sourceTreeTest/cocoapod.h
@@ -0,0 +1,13 @@
+//
+//  cocoapod.h
+//  sourceTreeTest
+//
+//  Created by QiLu on 14/09/2017.
+//  Copyright © 2017 tp-link. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cocoapod : NSObject
+- (void)cocoapod;
+@end
---
+++ sourceTreeTest/sourceTreeTest/cocoapod.m
@@ -0,0 +1,15 @@
+//
+//  cocoapod.m
+//  sourceTreeTest
+//
+//  Created by QiLu on 14/09/2017.
+//  Copyright © 2017 tp-link. All rights reserved.
+//
+
+#import "cocoapod.h"
+
+@implementation cocoapod
+- (void)cocoapod {
+    NSLog(@"%s", __func__);
+}
+@end
